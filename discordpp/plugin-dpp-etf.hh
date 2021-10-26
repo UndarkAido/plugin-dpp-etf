c400fe8f0610754c1f8101f5c20741f3dc83bf47
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2021-10-25 23:50:11 -0500

    Reformat

--- discordpp/plugin-dpp-etf.hh
+++ discordpp/plugin-dpp-etf.hh
@@ -1,22 +1,24 @@
+//
+// Created by Aidan on 10/25/2021.
+//
+
 #pragma once
 
 #include <dpp/etf.h>
 
-namespace discordpp{
-	template<class BASE>
-	class PluginDppETF: public BASE, virtual BotStruct{
-            dpp::etf_parser parser;
-          public:
-            PluginDppETF() : BASE() {
-                BASE::encoding_ = "etf";
-            }
+namespace discordpp {
+template <class BASE> class PluginDppETF : public BASE, virtual BotStruct {
+    dpp::etf_parser parser;
 
-          protected:
-            json parsePayload(const std::string &payload) override {
-                return parser.parse(payload);
-            }
-            std::string buildPayload(const json& payload) override{
-                return parser.build(payload);
-            }
-        };
-}
+  public:
+    PluginDppETF() : BASE() { BASE::encoding_ = "etf"; }
+
+  protected:
+    json parsePayload(const std::string &payload) override {
+        return parser.parse(payload);
+    }
+    std::string buildPayload(const json &payload) override {
+        return parser.build(payload);
+    }
+};
+} // namespace discordpp
