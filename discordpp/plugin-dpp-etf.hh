2509889666f487020b91f0c6a091a49cf0f9b50d
Aidan Edwards <Aidan.S.Edwards@gmail.com>
2021-10-25 20:25:19 -0500

    Initial implementation

---
+++ discordpp/plugin-dpp-etf.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <dpp/etf.h>
+
+namespace discordpp{
+	template<class BASE>
+	class PluginDppETF: public BASE, virtual BotStruct{
+            dpp::etf_parser parser;
+          public:
+            PluginDppETF() : BASE() {
+                BASE::encoding_ = "etf";
+            }
+
+          protected:
+            json parsePayload(const std::string &payload) override {
+                return parser.parse(payload);
+            }
+            std::string buildPayload(const json& payload) override{
+                return parser.build(payload);
+            }
+        };
+}
